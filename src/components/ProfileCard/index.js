af4f947eb09e5ed571528d60b9ff950317fd33f4
Frank-Colorado <chawnkiee@gmail.com>
2023-07-23 21:40:37 -0400

    created ProfileCard component

---
+++ src/components/ProfileCard/index.js
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  IconButton,
+  CardActions,
+  CardActionArea,
+  Link,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const ProfileCard = ({ name, image, github, website }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <Link
+          color="inherit"
+          underline="none"
+          href={website}
+          target="_blank"
+          style={{ textDecoration: "none" }}
+        >
+          <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt="{name} picture"
+          />
+        </Link>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <IconButton aria-label="github" component="a" href={github}>
+          <GitHubIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProfileCard;
