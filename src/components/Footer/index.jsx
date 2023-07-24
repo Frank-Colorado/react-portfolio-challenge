b119d320fb22ef5a56bd42959ad5ef02833e05e4
Frank-Colorado <chawnkiee@gmail.com>
2023-07-23 23:05:46 -0400

    finished Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,18 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
+import footerIcons from "../../utils/footerIcons";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
+  const iconStyle = {
+    width: "3rem",
+    height: "3rem",
+    margin: "1.5rem",
+  };
+
   return (
     <AppBar
       style={{
@@ -24,33 +34,29 @@ const Footer = () => {
               justifyContent: "center",
             }}
           >
-            <IconButton aria-label="GitHub" color="inherit" href="">
-              <GitHubIcon
+            {footerIcons.map((icon, index) => (
+              <IconButton
+                aria-label="GitHub"
+                color="inherit"
+                href={icon.link}
+                key={index}
+                target="_blank"
                 sx={{
-                  width: "3rem",
-                  height: "3rem",
-                  margin: "1.5rem",
+                  "&:hover": {
+                    transform: "scale(1.2)",
+                    transition: "transform 0.5s ease-in",
+                  },
                 }}
-              />
-            </IconButton>
-            <IconButton aria-label="LinkedIn" color="inherit" href="">
-              <LinkedInIcon
-                sx={{
-                  width: "3rem",
-                  height: "3rem",
-                  margin: "1.5rem",
-                }}
-              />
-            </IconButton>
-            <IconButton size="large" aria-label="Email" color="inherit" href="">
-              <EmailIcon
-                sx={{
-                  width: "3rem",
-                  height: "3rem",
-                  margin: "1.5rem",
-                }}
-              />
-            </IconButton>
+              >
+                {icon.icon === "GitHub" ? (
+                  <GitHubIcon sx={iconStyle} />
+                ) : icon.icon === "LinkedIn" ? (
+                  <LinkedInIcon sx={iconStyle} />
+                ) : (
+                  <EmailIcon sx={iconStyle} />
+                )}
+              </IconButton>
+            ))}
           </Box>
         </Toolbar>
       </Container>
---
+++ src/utils/footerIcons/index.js
@@ -0,0 +1,16 @@
+const footerIcons = [
+  {
+    icon: "GitHub",
+    link: "",
+  },
+  {
+    icon: "LinkedIn",
+    link: "",
+  },
+  {
+    icon: "Email",
+    link: "",
+  },
+];
+
+export default footerIcons;
