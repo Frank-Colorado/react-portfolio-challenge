eb71182d14712f12736ef2a9b39e83f27d31f033
Frank-Colorado <chawnkiee@gmail.com>
2023-07-23 21:31:29 -0400

    created ProfileCard component folder

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,12 +1,5 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
-import image from "../../assets/me.jpg";
 
 const Portfolio = () => {
   return (
