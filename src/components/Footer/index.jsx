562a885218a8a83ed6655627d7cbe82e37e8075d
Frank-Colorado <chawnkiee@gmail.com>
2023-07-23 22:54:41 -0400

    created footerIcons in utils

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
