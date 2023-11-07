720f39e622c2f4fc6f54f716ca2dd5808108a39f
Frank-Colorado <chawnkiee@gmail.com>
2023-11-07 16:48:46 -0500

    updates to variables in project list

--- src/utils/projectList/index.js
+++ src/utils/projectList/index.js
@@ -1,46 +1,46 @@
-import getgoodgames from "../../assets/getgoodgames.png";
-import richlifestyle from "../../assets/richlifestyle.png";
-import techblog from "../../assets/techblog.png";
-import notetaker from "../../assets/notetaker.png";
-import codequizgame from "../../assets/codequizgame.png";
-import soccerscout from "../../assets/soccerscout.png";
+import getGoodGames from '../../assets/getgoodgames.png';
+import richLifestyle from '../../assets/richlifestyle.png';
+import techBlog from '../../assets/techblog.png';
+import noteTaker from '../../assets/notetaker.png';
+import codeQuizGame from '../../assets/codequizgame.png';
+import soccerScout from '../../assets/soccerscout.png';
 
 const projects = [
   {
-    name: "Get Good Games",
-    image: getgoodgames,
-    github: "https://github.com/Frank-Colorado/get-good-games.git",
-    website: "https://frank-colorado.github.io/get-good-games/",
+    name: 'Get Good Games',
+    image: getGoodGames,
+    github: 'https://github.com/Frank-Colorado/get-good-games.git',
+    website: 'https://frank-colorado.github.io/get-good-games/',
   },
   {
-    name: "Rich Life Style",
-    image: richlifestyle,
-    github: "https://github.com/Frank-Colorado/rich-life-style.git",
-    website: "https://rich-life-style-f9caa98d3d2e.herokuapp.com/",
+    name: 'Rich Life Style',
+    image: richLifestyle,
+    github: 'https://github.com/Frank-Colorado/rich-life-style.git',
+    website: 'https://rich-life-style-f9caa98d3d2e.herokuapp.com/',
   },
   {
-    name: "Soccer Scout",
-    image: soccerscout,
-    github: "https://github.com/demiapollo/soccerScout.git",
-    website: "https://soccerscout-c2b068cbb836.herokuapp.com/",
+    name: 'Soccer Scout',
+    image: soccerScout,
+    github: 'https://github.com/demiapollo/soccerScout.git',
+    website: 'https://soccerscout-c2b068cbb836.herokuapp.com/',
   },
   {
-    name: "Tech Blog",
-    image: techblog,
-    github: "https://github.com/Frank-Colorado/tech-blog-challenge.git",
-    website: "https://tech-blog-challenge14.herokuapp.com/",
+    name: 'Tech Blog',
+    image: techBlog,
+    github: 'https://github.com/Frank-Colorado/tech-blog-challenge.git',
+    website: 'https://tech-blog-challenge14.herokuapp.com/',
   },
   {
-    name: "Note Taker",
-    image: notetaker,
-    github: "https://github.com/Frank-Colorado/note-taker-challenge.git",
-    website: "https://note-taker-challenge.herokuapp.com/notes",
+    name: 'Note Taker',
+    image: noteTaker,
+    github: 'https://github.com/Frank-Colorado/note-taker-challenge.git',
+    website: 'https://note-taker-challenge.herokuapp.com/notes',
   },
   {
-    name: "Code Quiz Game",
-    image: codequizgame,
-    github: "https://github.com/Frank-Colorado/code-quiz-game.git",
-    website: "https://frank-colorado.github.io/code-quiz-game/",
+    name: 'Code Quiz Game',
+    image: codeQuizGame,
+    github: 'https://github.com/Frank-Colorado/code-quiz-game.git',
+    website: 'https://frank-colorado.github.io/code-quiz-game/',
   },
 ];
 
