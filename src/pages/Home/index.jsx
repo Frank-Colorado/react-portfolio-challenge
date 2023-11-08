2dfe3bfa3e547b4d5779a3f305abe93c3d517f38
Frank-Colorado <chawnkiee@gmail.com>
2023-11-07 22:27:26 -0500

    small changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,25 @@
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
-import profileImage from "../../assets/me.jpg";
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import profileImage from '../../assets/me.jpg';
 
 const Home = () => {
   return (
     <div
       style={{
-        height: "100vh",
+        height: '100vh',
       }}
     >
       <Typography
         variant="h3"
         align="center"
         sx={{
-          fontFamily: "Constantia Regular",
+          fontFamily: 'Constantia Regular',
           fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-          marginTop: "5rem",
+          letterSpacing: '.3rem',
+          color: 'inherit',
+          textDecoration: 'none',
+          marginTop: '5rem',
         }}
       >
         About Me
@@ -29,38 +29,38 @@ const Home = () => {
         direction="row"
         spacing={3}
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          alignContent: "center",
-          height: "55%",
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          alignContent: 'center',
+          height: '55%',
         }}
       >
         <Avatar
           alt="Frank Colorado"
           src={profileImage}
           sx={{
-            width: "20%",
-            height: "75%",
-            marginRight: "5rem",
+            width: '20%',
+            height: '75%',
+            marginRight: '5rem',
           }}
         />
         <Typography
           paragraph
           href="/"
           sx={{
-            fontFamily: "Constantia Regular",
+            fontFamily: 'Constantia Regular',
             fontWeight: 500,
-            fontSize: "1.5rem",
-            letterSpacing: ".2rem",
-            color: "inherit",
-            textDecoration: "none",
-            width: "30%",
+            fontSize: '1.5rem',
+            letterSpacing: '.2rem',
+            color: 'inherit',
+            textDecoration: 'none',
+            width: '30%',
           }}
           align="center"
         >
-          Frank Colorado is an up and coming full stack web developer based in
-          Tampa, Florida. You can find his portfolio highlighting some of his
+          Frank Colorado is an up and coming full stack web developer working
+          completely remote. You can find his portfolio highlighting some of his
           development work and his resume throughout the site, as well as links
           to platforms where you can find him and his work in the footer.
         </Typography>
